e3b19c7b90be7359d341aecd0096699a310b2740
Tomas <mistuna.tomas@gmail.com>
2025-11-16 18:45:07 +0100

    arrays2

--- Arrays/test.cpp
+++ Arrays/test.cpp
@@ -4,9 +4,32 @@
 int main() 
 {
       using namespace std;
-      string cars [3] = {"Volvo", "BMW", "Ford", "Mazda"};
+      /*
+      string age [5] = {"29", "3", "51", "32", "63"};
+
+      double sum_arrays = 0;
+      double sum_age = 0;
+
+      for (int i = 0; i < 5; i++) 
+      {
+          sum_arrays = stoi(age[i]);
+          sum_age = sum_age + sum_arrays;
+      }
+
+      double avg_age = sum_age / 5;
       
-      cout << cars[3];
+      cout << "Average age is:" << avg_age;
+
+      */
+
+      //2D array
+      int arr[3][4] =
+      {
+          {1, 2, 3, 4},
+          {5, 6, 7, 8},
+          {9, 10, 11, 12}
+      };
+      cout << arr[1][1]; //prints 12
 
       return 0;
 }
