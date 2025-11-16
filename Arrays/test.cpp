d672594b8de9b9231eb608864ad3bc8b5ae70f10
Tomas <mistuna.tomas@gmail.com>
2025-11-16 18:34:12 +0100

    arrays

---
+++ Arrays/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream> //input, output stream
+#include <string>   //string class
+#include <math.h>
+int main() 
+{
+      using namespace std;
+      string cars [3] = {"Volvo", "BMW", "Ford", "Mazda"};
+      
+      cout << cars[3];
+
+      return 0;
+}
--- MemoryAlocation/main.cpp
+++ MemoryAlocation/main.cpp
@@ -5,9 +5,11 @@ int main ()
 {
     using namespace std;
 
-int * p_number = new int; // allocate an integer
+    int * p_number = new int; // allocate an integer
     *p_number = 100; // assign a value to the allocated memory
+    
     cout << "Value of p_number: " << *p_number << endl;
+    
     delete p_number; // free the allocated memory
     p_number = nullptr; // set pointer to nullptr after deletion
     
